d83b1f519b1ddfa2c2add8bc4d26a264c78266b7
rylero <leroy.ryan09@gmail.com>
2024-01-06 14:01:30 -0800

    bugfix

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ let lastClick;
 let time = 0;
 let dist = 0;
 let speed = 1; // feet per second
+let textBoxElem;
 
 function preload() {
     fieldImg = loadImage('field.png');
+    textBoxElem = document.getElementById("resultsDiv");
 }
 
-function addTime() {
+function addTime(mult) {
     var startX = actions[actions.length-1].start.x;
     var startY = actions[actions.length-1].start.y;
     var endX = actions[actions.length-1].end.x;
@@ -18,8 +20,8 @@ function addTime() {
 
     var distT = sqrt((startX-endX)*(startX-endX) + (startY-endY)*(startY-endY)) * 54/(1544*scale);
 
-    dist += distT;
-    time += distT / speed;
+    dist += distT * mult;
+    time += (distT / speed) * mult;
 }
 
 function setup() {
@@ -39,13 +41,29 @@ function setup() {
             "start": {"x":lastClick.x, "y":lastClick.y},
             "end": {"x":mouseX, "y":mouseY}
         })
-        addTime()
+        addTime(1)
 
         lastClick = {"x":mouseX, "y":mouseY};
     })
 }
 
 function keyPressed() {
+    if (keyCode == 48) {
+        var wtime = 0.5;
+        actions.push({
+            "type": "line",
+            "start": {"x":lastClick.x, "y":lastClick.y},
+            "end": {"x":mouseX, "y":mouseY}
+        })
+        addTime(1)
+        lastClick = {"x":mouseX, "y":mouseY};
+        actions.push({
+            "type": "wait",
+            "time": wtime,
+            "end": {"x":lastClick.x, "y":lastClick.y}
+        })
+        time += wtime;
+    }
     if (keyCode > 48 && keyCode < 58) {
         var wtime = keyCode - 48;
         actions.push({
@@ -53,7 +71,7 @@ function keyPressed() {
             "start": {"x":lastClick.x, "y":lastClick.y},
             "end": {"x":mouseX, "y":mouseY}
         })
-        addTime()
+        addTime(1)
         lastClick = {"x":mouseX, "y":mouseY};
         actions.push({
             "type": "wait",
@@ -63,8 +81,22 @@ function keyPressed() {
         time += wtime;
     }
     if (keyCode == 85) {
+        if (actions.length == 0) {
+            lastClick = undefined;
+            time = 0;
+            dist = 0;
+        }
+        if (actions.length >= 1) {
+            if (actions[actions.length-1].type == "wait") {
+                time -= actions[actions.length-1].time;
+            }
+        }
+        if (actions.length == 1) {
+            lastClick = {"x":actions[actions.length-1].start.x,"y":actions[actions.length-1].start.y}
+        } else {
+            lastClick = {"x":actions[actions.length-2].end.x,"y":actions[actions.length-2].end.y}
+        }
         actions.pop();
-        lastClick = {"x":actions[actions.length-1].end.x,"y":actions[actions.length-1].end.y}
     }
   }
 
@@ -74,8 +106,7 @@ function draw() {
 
     fill(255,255,255);
     textSize(30);
-    text("Cycle Time: "+Math.round(time * 100) / 100, 10, 30);
-    text("Distance: "+Math.round(dist * 100) / 100, 10, 70);
+    textBoxElem.innerHTML = "<h1>Cycle Time: "+Math.round(time * 100) / 100+"</h1><h1>Distance: "+Math.round(dist * 100) / 100+"</h1>";
 
     if (actions.length > 0) {
         for (var i = 0; i < actions.length; i++) {
@@ -89,7 +120,7 @@ function draw() {
                 ellipse(a.end.x, a.end.y, 30, 30);
                 fill(0,0,0);
                 textSize(16);
-                text(a.time, a.end.x-5, a.end.y+2);
+                text(a.time, a.end.x-7, a.end.y+2);
             }
         }
 
